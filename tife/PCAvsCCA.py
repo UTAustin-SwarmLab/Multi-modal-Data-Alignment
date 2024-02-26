62a75f2f78dc6170c33fc8a214cd9aeaff40916e
d31003 <pohanli@utexas.edu>
2024-02-25 22:54:43 -0600

    Add hydra configuration and refactor code for PCA and CCA

--- tife/PCAvsCCA.py
+++ tife/PCAvsCCA.py
@@ -1,74 +1,77 @@
 import pickle
 
+import hydra
 import numpy as np
 from cca_zoo.linear import CCA
+from omegaconf import DictConfig
 from sklearn.decomposition import PCA
 from utils.linear_algebra_utils import origin_centered
 from utils.SVM_classifier import SVM_classifier
 
-img_encoder = "clip"
-text_encoder = "clip"
 
-root_dir =  "/home/pl22767/Data/waterbird_landfraction50_forest2water2/"
-save_dir = "/nas/tirr/waterbird/"
+@hydra.main(version_base=None, config_path='config', config_name='main_config')
+def main(cfg: DictConfig):
+    # load waterbirds embeddings
+    with open(cfg.save_dir + f'data/waterbird_img_emb_train_test_{cfg.img_encoder}.pkl', 'rb') as f:
+        dbFeatImg = pickle.load(f)
+    with open(cfg.save_dir + f'data/waterbird_img_emb_val_{cfg.img_encoder}.pkl', 'rb') as f:
+        qFeatImg = pickle.load(f)
+    with open(cfg.save_dir + f'data/waterbird_text_emb_train_test_{cfg.text_encoder}.pkl', 'rb') as f:
+        dbFeatText = pickle.load(f)
+    with open(cfg.save_dir + f'data/waterbird_text_emb_val_{cfg.text_encoder}.pkl', 'rb') as f:
+        qFeatText = pickle.load(f)
 
-# load waterbirds embeddings
-with open(save_dir + f'data/waterbird_img_emb_train_test_{img_encoder}.pkl', 'rb') as f:
-    dbFeatImg = pickle.load(f)
-with open(save_dir + f'data/waterbird_img_emb_val_{img_encoder}.pkl', 'rb') as f:
-    qFeatImg = pickle.load(f)
-with open(save_dir + f'data/waterbird_text_emb_train_test_{text_encoder}.pkl', 'rb') as f:
-    dbFeatText = pickle.load(f)
-with open(save_dir + f'data/waterbird_text_emb_val_{text_encoder}.pkl', 'rb') as f:
-    qFeatText = pickle.load(f)
+    # zero-mean data
+    dbFeatImg, dbFeatImg_mean = origin_centered(dbFeatImg)
+    dbFeatText, dbFeatText_mean = origin_centered(dbFeatText)
+    qFeatImg = qFeatImg - dbFeatImg_mean
+    qFeatText = qFeatText - dbFeatText_mean
+    # qFeatImg, _ = origin_centered(qFeatImg)
+    # qFeatText, _ = origin_centered(qFeatText)
+    print(dbFeatImg.shape, dbFeatText.shape, qFeatImg.shape, qFeatText.shape)
+    print(dbFeatImg_mean.shape, dbFeatText_mean.shape)
 
-# zero-mean data
-dbFeatImg, dbFeatImg_mean = origin_centered(dbFeatImg)
-dbFeatText, dbFeatText_mean = origin_centered(dbFeatText)
-qFeatImg = qFeatImg - dbFeatImg_mean
-qFeatText = qFeatText - dbFeatText_mean
-# qFeatImg, _ = origin_centered(qFeatImg)
-# qFeatText, _ = origin_centered(qFeatText)
-print(dbFeatImg.shape, dbFeatText.shape, qFeatImg.shape, qFeatText.shape)
-print(dbFeatImg_mean.shape, dbFeatText_mean.shape)
-
-# load ground truth
-with open(save_dir + 'data/waterbird_gt_train_test.pkl', 'rb') as f:
-    gt_train_test = pickle.load(f)
-gt_train_test = np.array([x[1] for x in gt_train_test])
-with open(save_dir + 'data/waterbird_gt_val.pkl', 'rb') as f:
-    gt_val = pickle.load(f)
-gt_val = np.array([_[1] for _ in gt_val])
+    # load ground truth
+    with open(cfg.save_dir + 'data/waterbird_gt_train_test.pkl', 'rb') as f:
+        gt_train_test = pickle.load(f)
+    gt_train_test = np.array([x[1] for x in gt_train_test])
+    with open(cfg.save_dir + 'data/waterbird_gt_val.pkl', 'rb') as f:
+        gt_val = pickle.load(f)
+    gt_val = np.array([_[1] for _ in gt_val])
 
 
-# PCA and CCA
-for dim in range(50, 701, 50):
-    print("Embedding Dimension:", dim)
+    # PCA and CCA
+    for dim in range(50, 701, 50):
+        print("Embedding Dimension:", dim)
 
-    # fit CCA and PCA
-    img_text_CCA = CCA(latent_dimensions=dim)
-    img_text_CCA.fit((dbFeatImg, dbFeatText))
-    imgPca = PCA(n_components=dim)
-    imgPca.fit(dbFeatImg)
+        # fit CCA and PCA
+        img_text_CCA = CCA(latent_dimensions=dim)
+        img_text_CCA.fit((dbFeatImg, dbFeatText))
+        imgPca = PCA(n_components=dim)
+        imgPca.fit(dbFeatImg)
 
-    # calculate PCA
-    pca_dbFeatImg = imgPca.transform(dbFeatImg)[:,:dim]
-    pca_qFeatImg = imgPca.transform(qFeatImg)[:,:dim]
-    
-    # fit PCA SVM and calculate accuracy
-    svm = SVM_classifier(pca_dbFeatImg, gt_train_test)
-    qPred = svm.predict(pca_qFeatImg)
-    dbAcc = svm.get_accuracy(svm.y_pred, gt_train_test)
-    qAcc = svm.get_accuracy(qPred, gt_val)
-    print("PCA train SVM Accuracy", dbAcc, "PCA val SVM Accuracy", qAcc)
+        # calculate PCA
+        pca_dbFeatImg = imgPca.transform(dbFeatImg)[:,:dim]
+        pca_qFeatImg = imgPca.transform(qFeatImg)[:,:dim]
+        
+        # fit PCA SVM and calculate accuracy
+        svm = SVM_classifier(pca_dbFeatImg, gt_train_test)
+        qPred = svm.predict(pca_qFeatImg)
+        dbAcc = svm.get_accuracy(svm.y_pred, gt_train_test)
+        qAcc = svm.get_accuracy(qPred, gt_val)
+        print("PCA train SVM Accuracy", dbAcc, "PCA val SVM Accuracy", qAcc)
 
-    # calculate CCA
-    cca_dbFeatImg, cca_dbFeatText = img_text_CCA.transform((dbFeatImg, dbFeatText))
-    cca_qFeatImg, cca_qFeatText = img_text_CCA.transform((qFeatImg, qFeatText))
+        # calculate CCA
+        cca_dbFeatImg, cca_dbFeatText = img_text_CCA.transform((dbFeatImg, dbFeatText))
+        cca_qFeatImg, cca_qFeatText = img_text_CCA.transform((qFeatImg, qFeatText))
 
-    # fit SVM
-    svm = SVM_classifier(cca_dbFeatImg, gt_train_test)
-    qPred = svm.predict(cca_qFeatImg)
-    dbAcc = svm.get_accuracy(svm.y_pred, gt_train_test)
-    qAcc = svm.get_accuracy(qPred, gt_val)
-    print("CCA train SVM Accuracy", dbAcc, "CCA val SVM Accuracy", qAcc)
+        # fit SVM
+        svm = SVM_classifier(cca_dbFeatImg, gt_train_test)
+        qPred = svm.predict(cca_qFeatImg)
+        dbAcc = svm.get_accuracy(svm.y_pred, gt_train_test)
+        qAcc = svm.get_accuracy(qPred, gt_val)
+        print("CCA train SVM Accuracy", dbAcc, "CCA val SVM Accuracy", qAcc)
+
+
+if __name__ == '__main__':
+    main()
