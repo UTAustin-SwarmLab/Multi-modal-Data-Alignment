4b92253c55272e113975892c4bc52d3a4f1fe81b
Po-han Li <34739111+d31003@users.noreply.github.com>
2024-02-26 11:48:24 +0800

    Initial commit

---
+++ tests/subpackage_name/test_subpackage_module_name.py
@@ -0,0 +1,10 @@
+"""test script of subpackage module."""
+
+from your_project_name.subpackage_name.subpackage_module_name import (
+    subpackage_function,
+)
+
+
+def test_subpackage_function():
+    """Test subpackage function."""
+    assert subpackage_function() is True
---
+++ your_project_name/subpackage_name/subpackage_module_name.py
@@ -0,0 +1,7 @@
+"""subpackage module."""
+
+
+def subpackage_function():
+    """subpackage function."""
+    print("Hello World")
+    return True
