2488defd0cae6154799108dc57a5c053ed39b1e5
d31003 <pohanli@utexas.edu>
2024-04-08 15:35:44 -0500

    Add test scripts and modify main.yaml

---
+++ scripts/test.py
@@ -0,0 +1,19 @@
+import json
+import os
+
+from omegaconf import DictConfig
+
+import hydra
+
+
+@hydra.main(version_base=None, config_path="../config", config_name="main")
+def main(cfg: DictConfig):  # noqa: D103
+    full_test_json = json.load(open(os.path.join(cfg.tiil.paths.dataset_path + "full_test.json")))
+    import pdb
+
+    pdb.set_trace()
+    return
+
+
+if __name__ == "__main__":
+    main()
