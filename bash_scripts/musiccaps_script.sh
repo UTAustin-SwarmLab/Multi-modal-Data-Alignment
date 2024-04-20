f93050700e09decd87181ef709f25131366cb857
d31003 <pohanli@utexas.edu>
2024-04-19 23:02:57 -0500

    Add models' embedding sizes

--- bash_scripts/musiccaps_script.sh
+++ bash_scripts/musiccaps_script.sh
@@ -1,11 +1,11 @@
 # musiccaps
-# poetry run python mmda/bimodal_shuffled_data.py dataset=musiccaps musiccaps.sim_dim=10
-# poetry run python mmda/bimodal_shuffled_data.py dataset=musiccaps musiccaps.sim_dim=25
-# poetry run python mmda/bimodal_shuffled_data.py dataset=musiccaps musiccaps.sim_dim=50
-# poetry run python mmda/bimodal_shuffled_data.py dataset=musiccaps musiccaps.sim_dim=100
-# poetry run python mmda/bimodal_shuffled_data.py dataset=musiccaps musiccaps.sim_dim=150
-# poetry run python mmda/bimodal_shuffled_data.py dataset=musiccaps musiccaps.sim_dim=200
-# poetry run python mmda/bimodal_shuffled_data.py dataset=musiccaps musiccaps.sim_dim=500
+poetry run python mmda/bimodal_shuffled_data.py dataset=musiccaps musiccaps.sim_dim=10
+poetry run python mmda/bimodal_shuffled_data.py dataset=musiccaps musiccaps.sim_dim=25
+poetry run python mmda/bimodal_shuffled_data.py dataset=musiccaps musiccaps.sim_dim=50
+poetry run python mmda/bimodal_shuffled_data.py dataset=musiccaps musiccaps.sim_dim=100
+poetry run python mmda/bimodal_shuffled_data.py dataset=musiccaps musiccaps.sim_dim=150
+poetry run python mmda/bimodal_shuffled_data.py dataset=musiccaps musiccaps.sim_dim=200
+poetry run python mmda/bimodal_shuffled_data.py dataset=musiccaps musiccaps.sim_dim=500
 poetry run python mmda/bimodal_shuffled_data.py dataset=musiccaps train_test_ratio=0.15 musiccaps.sim_dim=10
 poetry run python mmda/bimodal_shuffled_data.py dataset=musiccaps train_test_ratio=0.15 musiccaps.sim_dim=25
 poetry run python mmda/bimodal_shuffled_data.py dataset=musiccaps train_test_ratio=0.15 musiccaps.sim_dim=50
