90459686ac455063b3ef9e3c8e33517492028da0
d31003 <pohanli@utexas.edu>
2024-10-08 12:16:46 -0500

    Binary classification conformal prediction done on jupyter

--- mmda/utils/calibrate.py
+++ mmda/utils/calibrate.py
@@ -6,12 +6,29 @@ import numpy as np
 from tqdm import tqdm
 
 
-def con_mat_calibrate(sim_mat_dict: dict, nc_scores: dict) -> dict:
+def calibration_score(
+    x: np.ndarray, gt: np.ndarray, min_score: float, max_score: float
+) -> np.ndarray:
+    """Calculate the calibration score.
+
+    Args:
+        x: the similarity score. shape: (n)
+        gt: the ground truth label. shape: (n)
+        min_score: the minimum score. shape: (n)
+        max_score: the maximum score. shape: (n)
+
+    Returns:
+        The calibration score. shape: (n)
+    """
+    return np.abs(gt - (x - min_score) / (max_score - min_score))
+
+
+def con_mat_calibrate(sim_mat_dict: dict, scores: dict) -> dict:
     """Calculate the conformal matrix from similarity matirx.
 
     Args:
         sim_mat_dict: the similarity matrix. dict: (i, j) -> (similarity matrix, ground truth)
-        nc_scores: the nonconformity scores. dict: (i, j) -> list[float]
+        scores: the nonconformity scores. dict: (i, j) -> list[float]
 
     Returns:
         The conformal matrix. dict: (i, j) -> (conformal matrix, ground truth)
@@ -25,103 +42,90 @@ def con_mat_calibrate(sim_mat_dict: dict, nc_scores: dict) -> dict:
         probs = np.zeros((3, 3))
         for i in range(3):
             for j in range(3):
-                probs[i, j] = calibrate(sim_mat[i, j], nc_scores[(i, j)])
+                probs[i, j] = calibrate(sim_mat[i, j], scores[(i, j)])
         con_mat[(idx_q, idx_r)] = (probs, gt_label)
     return con_mat
 
 
-def get_non_conformity_scores_common(
-    q2scores: dict, top_k: int = 5
-) -> tuple[list[float], list[float], dict]:
-    """Get the nonconformity scores for the quenry index dict."""
-    for q in q2scores:
-        q2scores[q] = np.array(q2scores[q])
-    for q in q2scores:
-        q2scores[q] = q2scores[q][np.argsort(-q2scores[q][:, 0])]
-        q2scores[q] = (
-            q2scores[q][top_k - 1][0],  # the score of the top k-th pair
-            int(
-                sum(q2scores[q][:top_k, 1]) >= 1.0
-            ),  # if there is at least one positive pair in the top k
-        )
-    nc_scores = []
-    c_scores = []
-    for q in q2scores:
-        if q2scores[q][1] == 1.0:
-            c_scores.append(q2scores[q][0])
-        else:
-            nc_scores.append(q2scores[q][0])
-    # sort the scores
-    nc_scores.sort()
-    c_scores.sort()
-    return nc_scores, c_scores, q2scores
-
-
-def get_non_conformity_scores_1st_stage(
-    sim_mat: dict, idx_modal1: int, idx_modal2: int, top_k: int = 5
-) -> tuple[list[float], list[float], dict]:
+def get_calibration_scores_common(
+    sim_scores: list[float], gt_labels: list[int]
+) -> tuple[list[float], list[int]]:
     """Get the nonconformity scores for the given modalities.
 
     Args:
-        sim_mat: the similarity matrix. dict: (i, j) -> (similarity matrix, ground truth)
-        idx_modal1: index of the first modality
-        idx_modal2: index of the second modality
-        top_k: the number of top scores to consider for recall
+        sim_scores: the similarity scores
+        gt_labels: the ground truth labels
 
     Returns:
-        nc_scores: the nonconformity scores
-        c_scores: the conformity scores
-        q2scores: the query index dict i -> (top k-th sim score, label)
+        cali_scores: the SORTED distribution of calibration scores
+        gt_labels: the ground truth labels
     """
-    q2scores = {}  # dict: query idx -> (top k-th sim score, label)
-    for q, r in sim_mat:
+    sim_scores = np.array(sim_scores).reshape(-1)
+    gt_labels = np.array(gt_labels).reshape(-1)
+    # calculate the calibration scores
+    cali_scores = calibration_score(
+        np.array(sim_scores), np.array(gt_labels), min(sim_scores), max(sim_scores)
+    )
+    assert cali_scores.shape == sim_scores.shape
+    # ascending order
+    return sorted(cali_scores.reshape(-1).tolist()), gt_labels
+
+
+def get_calibration_scores_1st_stage(
+    sim_mat_dict: dict, idx_modal1: int, idx_modal2: int
+) -> tuple[list[float], list[int]]:
+    """Get the nonconformity scores for the given modalities.
+
+    Args:
+        sim_mat_dict: the similarity matrix. dict: (i, j) -> (similarity matrix, ground truth)
+        idx_modal1: index of the first modality
+        idx_modal2: index of the second modality
+
+    Returns:
+        cali_scores: the SORTED distribution of calibration scores
+        gt_labels: the ground truth labels
+    """
+    sim_scores = []
+    gt_labels = []
+    for (idx_q, idx_r), (sim_mat, gt_label) in sim_mat_dict.items():
         # skip the same pair of query and reference data
-        if q == r:
+        if idx_q == idx_r:
             continue
-        if q not in q2scores:
-            q2scores[q] = []
-        q2scores[q].append((sim_mat[q, r][0][idx_modal1][idx_modal2], sim_mat[q, r][1]))
-        if r not in q2scores:
-            q2scores[r] = []
-        q2scores[r].append((sim_mat[q, r][0][idx_modal1][idx_modal2], sim_mat[q, r][1]))
-    return get_non_conformity_scores_common(q2scores, top_k)
+        sim_scores.append(sim_mat[idx_modal1, idx_modal2])
+        gt_labels.append(gt_label)
+    return get_calibration_scores_common(sim_scores, gt_labels)
 
 
-def get_non_conformity_scores_2nd_stage(
-    con_mat: dict, mapping_fn: callable, top_k: int = 5
-) -> tuple[list[float], list[float], dict]:
+def get_calibration_scores_2nd_stage(
+    con_mat: dict, mapping_fn: callable
+) -> tuple[list[float], list[int]]:
     """Get the nonconformity scores for the given conformal matrices.
 
     Args:
         con_mat: the conformal matrix. dict: (i, j) -> (conformal matrix, ground truth)
         mapping_fn: the mapping function to map the conformal matrix to a scalar, e.g., max, mean
-        top_k: the number of top scores to consider for recall
 
     Returns:
-        nc_scores: the nonconformity scores
-        c_scores: the conformity scores
-        q2scores: the query index dict i -> (top k-th sim score, label)
+        cali_scores: the SORTED distribution of calibration scores
+        gt_labels: the ground truth labels
     """
-    q2scores = {}  # dict: query idx -> (top k-th sim score, label)
-    for q, r in con_mat:
+    sim_scores = []
+    gt_labels = []
+    for idx_q, idx_r in con_mat:
         # skip the same pair of query and reference data
-        if q == r:
+        if idx_q == idx_r:
             continue
-        if q not in q2scores:
-            q2scores[q] = []
-        q2scores[q].append((mapping_fn(con_mat[q, r][0]), con_mat[q, r][1]))
-        if r not in q2scores:
-            q2scores[r] = []
-        q2scores[r].append((mapping_fn(con_mat[q, r][0]), con_mat[q, r][1]))
-    return get_non_conformity_scores_common(q2scores, top_k)
+        sim_scores.append(mapping_fn(con_mat[(idx_q, idx_r)][0]))
+        gt_labels.append(con_mat[(idx_q, idx_r)][1])
+    return get_calibration_scores_common(sim_scores, gt_labels)
 
 
-def calibrate(score: float, nc_scores: list[float]) -> float:
+def calibrate(score: float, scores: list[float]) -> float:
     """Calibrate the score using the nonconformity scores.
 
     Args:
         score: the score to be calibrated
-        nc_scores: the nonconformity scores
+        scores: the nonconformity scores
 
     Returns:
         The calibrated score.
@@ -130,5 +134,5 @@ def calibrate(score: float, nc_scores: list[float]) -> float:
     # since nc_scores is sorted in ascending order,
     # the calibrated score is the index divided by the total number of scores
     # we can use binary search to find the index of score in nc_scores
-    assert nc_scores[0] <= nc_scores[-1], "nc_scores should be in ascending order"
-    return bisect_left(nc_scores, score) / len(nc_scores)
+    assert scores[0] <= scores[-1], "nc_scores should be in ascending order"
+    return bisect_left(scores, score) / len(scores)
